7b114d3458125863212206d0dcafebd5ac507504
Adriano Gomes <adrianogsss@gmail.com>
2019-08-03 11:12:30 -0300

    feat: modelo da tabela profissao_autor do banco

---
+++ models/profissao_autor.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String
+import model
+
+
+class ProfissaoAutor(model.base):
+
+    """
+    
+    Modelo da tabela ProfissaoAutor
+
+    :param id_profissao_autor: <int>
+    :param profissao_autor: <string>
+
+    """
+
+    __tablename__ = "profissao_autor"
+
+    id_profissao_autor = Column(Integer, primary_key=True)
+    profissao_autor = Column(String)
+
+    def __repr__(self):
+        return "<Profissão Autr(id_profissao_autor='%s', profissao_autor='%s')>" % (
+            self.id_profissao_autor, self.profissao_autor)
+    
