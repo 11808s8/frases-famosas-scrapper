22ca1276669ee28d66d9886cdcb660ab57e26775
Adriano Gomes <adrianogsss@gmail.com>
2019-08-03 11:01:40 -0300

    fix: ajuste na linha de conexao para o banco local

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine  
 from sqlalchemy.orm import sessionmaker
 
-db_string = "postgres://postgres:postgres@localhost/samehours"
+db_string = "postgres://postgres:postgres@localhost/frases_scrapper"
 db = create_engine(db_string)  
 Session = sessionmaker(db)  
 session = Session()
