45027c0ea185e2aa6aefa0400d1ed666f46a03f2
Adriano Gomes <adrianogsss@gmail.com>
2019-08-04 16:52:14 -0300

    feat: adição de função para pegar a sequence do banco

--- ffs/db.py
+++ ffs/db.py
@@ -5,3 +5,13 @@ db_string = "postgres://postgres:postgres@localhost/frases_scrapper"
 db = create_engine(db_string)  
 Session = sessionmaker(db)  
 session = Session()
+
+
+# @TODO: Colocar um throw error aqui
+def last_sequence_number(table,schema='public'):
+    sequence_name = schema + '.' + table + '_id_' + table + 'seq'
+    select_sequence = 'select last_value from ' + sequence_name
+    seq = session.execute(select_sequence)
+    for retorno in seq:
+        return retorno[0]
+    return None
