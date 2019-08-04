0e6681210c3b2b82b94abcc00d767190e48bb715
Adriano Gomes <adrianogsss@gmail.com>
2019-08-04 11:32:34 -0300

    feat: adição da tabela M:M profissoes_autores

---
+++ models/profissoes_autores.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String
+import model
+
+
+class ProfissoesAutores(model.base):
+
+    """
+    
+    Modelo da tabela ProfissaoAutor
+
+    :param id_profissao_autor: <int>
+    :param id_autor: <int>
+
+    """
+
+    __tablename__ = "profissoes_autores"
+
+    id_profissao_autor = Column(Integer)
+    id_autor = Column(Integer)
+
+    def __repr__(self):
+        return "<Profissão Autr(id_profissao_autor='%s', profissao_autor='%s')>" % (
+            self.id_profissao_autor, self.profissao_autor)
+    
