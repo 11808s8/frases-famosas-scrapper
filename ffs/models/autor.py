933eb834a0a76a793a20a31106ef5b2ebee46606
Adriano Gomes <adrianogsss@gmail.com>
2019-08-08 21:32:23 -0300

    feat: adição de coluna de link alternativo d autor

--- ffs/models/autor.py
+++ ffs/models/autor.py
@@ -10,14 +10,16 @@ class Autor(model.base):
 
     :param id_autor: <int>
     :param autor: <string>
+    :param link_alternativo_autor: <string>
 
     """
     __tablename__ = 'autor'
 
     id_autor = Column(Integer, primary_key=True)
     autor = Column(String)
+    link_alternativo_autor = Column(String)
 
     def __repr__(self):
-        return "<Autor(id='%s', nome='%s')>" % (
-            self.id_autor, self.autor)
+        return "<Autor(id='%s', nome='%s', link alternativo='%s')>" % (
+            self.id_autor, self.autor, self.link_alternativo_autor)
     
