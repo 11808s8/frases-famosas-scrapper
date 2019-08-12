1adcf1d441dd2c61b0dae15ff1485f3b6c12728e
Adriano Gomes <adrianogsss@gmail.com>
2019-08-11 22:40:17 -0300

    feat: remoção de coluna de id_categoria da frase

--- ffs/models/frase.py
+++ ffs/models/frase.py
@@ -11,7 +11,6 @@ class Frase(model.base):
     :param id_frase: <int>
     :param frase: <string>
     :param id_autor: <int>
-    :param id_categoria: <int>
     :param date_created: <date>
     :param usado: <boolean>
 
@@ -21,11 +20,10 @@ class Frase(model.base):
     id_frase = Column(Integer, primary_key=True)
     frase = Column(String)
     id_autor = Column(Integer)
-    id_categoria = Column(Integer)
     date_created = Column(DateTime, default=datetime.now())
     usado = Column(Boolean)
 
     def __repr__(self):
-        return "<Frase(id='%s', frase='%s', id_autor='%s', id_categoria='%s', date_created='%s', usado='%s')>" % (
-            self.id_frase, self.frase, self.id_autor, self.id_categoria, self.date_created, self.usado)
+        return "<Frase(id='%s', frase='%s', id_autor='%s', date_created='%s', usado='%s')>" % (
+            self.id_frase, self.frase, self.id_autor, self.date_created, self.usado)
     
